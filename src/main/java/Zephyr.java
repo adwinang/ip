cc4eb452bde8a664f63019121e7435bcfeb73033
adwinang <adwin@aegir.co>
2025-01-23 18:29:34 +0800

    feat: Implement Zephyr Greet and Exit
    Duke file is renamed to Zephyr to follow the new name for the chatbot "Zephyr"
    
    Zephyr simply greets and exit immediately for now

---
+++ src/main/java/Zephyr.java
@@ -0,0 +1,12 @@
+public class Zephyr {
+    public static void main(String[] args) {
+        System.out.println("""
+                ____________________________________________________________
+                 Hello! I'm Zephyr
+                 What can I do for you?
+                ____________________________________________________________
+                 Bye. Hope to see you again soon!
+                ____________________________________________________________
+                """);
+    }
+}
