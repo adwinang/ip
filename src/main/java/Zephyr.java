116e001e6949ae25f0023bad9b6c4078d396af6e
adwinang <adwin@aegir.co>
2025-01-23 21:56:15 +0800

    feat: Add Task Tracker and Listing
    Case insensitive keyword "list" is used to list out all the task

--- src/main/java/Zephyr.java
+++ src/main/java/Zephyr.java
@@ -5,13 +5,17 @@ public class Zephyr {
                  Hello! I'm Zephyr
                  What can I do for thou?
                 ____________________________________________________________
-                
                 """;
         String goodbyeMessage= """
                 ____________________________________________________________
                  Bye. Hope to see thou again soon!
                 ____________________________________________________________
                 """;
+
+        // Assumption of max 100 tasks to store
+        String[] tasks = new String[100];
+        int taskCount = 0;
+
         System.out.println(greetingMessage);
 
         // Detect user input in cli
@@ -19,10 +23,23 @@ public class Zephyr {
         String userInput = scanner.nextLine();
         // Case is ignored to allow for case-insensitive input
         while (!userInput.equalsIgnoreCase("bye")) {
-            System.out.println("You said: " + userInput);
+            if (userInput.equalsIgnoreCase("list")) {
+                System.out.println("____________________________________________________________");
+                System.out.println("Here are the tasks in your list:");
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println(i + 1 + ". " + tasks[i]);
+                }
+                System.out.println("____________________________________________________________");
+            } else {
+                tasks[taskCount] = userInput;
+                taskCount++;
+                System.out.println("____________________________________________________________");
+                System.out.println("added: " + userInput);
+                System.out.println("____________________________________________________________");
+            }
             userInput = scanner.nextLine();
         }
-        System.out.println(goodbyeMessage);
 
+        System.out.println(goodbyeMessage);
     }
 }
