edfceacec3b35882fe38d772b0c342c827012cee
adwinang <adwin@aegir.co>
2025-01-23 21:37:00 +0800

    feat: Add echoing of user input and exit condition
    User can exit the system by typing case insensitive "bye"
    Change to usage of Archaic pronouns by Zephyr

--- src/main/java/Zephyr.java
+++ src/main/java/Zephyr.java
@@ -1,12 +1,28 @@
 public class Zephyr {
     public static void main(String[] args) {
-        System.out.println("""
+        String greetingMessage= """
                 ____________________________________________________________
                  Hello! I'm Zephyr
-                 What can I do for you?
+                 What can I do for thou?
                 ____________________________________________________________
-                 Bye. Hope to see you again soon!
+                
+                """;
+        String goodbyeMessage= """
                 ____________________________________________________________
-                """);
+                 Bye. Hope to see thou again soon!
+                ____________________________________________________________
+                """;
+        System.out.println(greetingMessage);
+
+        // Detect user input in cli
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+        String userInput = scanner.nextLine();
+        // Case is ignored to allow for case-insensitive input
+        while (!userInput.equalsIgnoreCase("bye")) {
+            System.out.println("You said: " + userInput);
+            userInput = scanner.nextLine();
+        }
+        System.out.println(goodbyeMessage);
+
     }
 }
