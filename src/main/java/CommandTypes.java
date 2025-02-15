c392386d65b92ada1e798a3af0502229e5954172
adwinang <adwin@aegir.co>
2025-02-15 19:23:07 +0800

    feat: Add CommandTypes Enum

---
+++ src/main/java/CommandTypes.java
@@ -0,0 +1,31 @@
+public enum CommandTypes {
+    LIST("list"),
+    MARK("mark"),
+    UNMARK("unmark"),
+    TODO("todo"),
+    DEADLINE("deadline"),
+    EVENT("event"),
+    DELETE("delete"),
+    UPCOMING("upcoming"),
+    BYE("bye"),
+    UNKNOWN("unknown");
+
+
+    private final String value;
+
+    // Constructor to associate string values
+    CommandTypes(String value) {
+        this.value = value;
+    }
+
+    // Method to get enum from string
+    public static CommandTypes fromValue(String value) {
+        for (CommandTypes command : CommandTypes.values()) {
+            if (command.value.equalsIgnoreCase(value)) {
+                return command;
+            }
+        }
+        return UNKNOWN;
+    }
+
+}
