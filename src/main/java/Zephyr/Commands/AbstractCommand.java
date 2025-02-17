c5b6f45e7fa7c34988d39310b8d150e42870724a
adwinang <adwin@aegir.co>
2025-02-17 21:53:05 +0800

    docs(AbstractCommand): Add Javadoc

--- src/main/java/Zephyr/Commands/AbstractCommand.java
+++ src/main/java/Zephyr/Commands/AbstractCommand.java
@@ -5,6 +5,9 @@ import Zephyr.DataStructures.TaskList;
 import Zephyr.Controllers.Ui;
 import Zephyr.Exceptions.ZephyrException;
 
+/**
+ * Abstract class for all commands
+ */
 public abstract class AbstractCommand {
     /**
      * Arguments of the command, excludes the command itself
@@ -12,13 +15,31 @@ public abstract class AbstractCommand {
     String arguments;
     String[] words;
 
+    /**
+     * Constructor for AbstractCommand
+     * Words array is populated with the arguments split by spaces
+     *
+     * @param arguments Arguments of the command
+     */
     AbstractCommand(String arguments) {
         this.arguments = arguments;
         this.words = this.arguments.split(" ");
     }
 
+    /**
+     * Executes the command
+     * @param tasks TaskList object
+     * @param ui Ui object
+     * @param storage Storage object
+     * @throws ZephyrException if the command is invalid
+     */
     public abstract void execute(TaskList tasks, Ui ui, Storage storage) throws ZephyrException;
 
+
+    /**
+     * Checks if the command is an exit command
+     * @return true if the command is an exit type command, false otherwise
+     */
     public boolean isExit() {
         return false;
     }
@@ -26,7 +47,7 @@ public abstract class AbstractCommand {
     /**
      * Checks if remaining arguments are valid
      * It is not context aware, meaning it does not check if the arguments are valid for Zephyr.TaskList, Zephyr.Controller.Ui or Zephyr.Controller.Storage
-     * Throws a Zephyr.Exceptions.ZephyrException if the command is invalid
+     * @throws ZephyrException if the command is invalid
      */
     public abstract void isValidCommand() throws ZephyrException;
 
